04e7f1a7ad150f670250a614fe7ad9b03e2dce2f
Konrad Linkowski <linkowski.konrad97@gmail.com>
2019-12-06 13:38:13 +0100

    2019/Day1

---
+++ 2019/Day1/b.js
@@ -0,0 +1,12 @@
+const input = require('../loader')
+const numbers = input.split('\n').map(Number)
+const calcFuel = mass => {
+  const fuel = Math.floor(mass / 3) - 2
+  if (fuel <= 0) return 0
+  return fuel + calcFuel(fuel)
+}
+const result = numbers.reduce((p, c) => {
+  const fuel = calcFuel(c)
+  return p + fuel
+}, 0)
+console.log(result)
