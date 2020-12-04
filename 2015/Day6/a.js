082fd272de3d68d162bd40f43dcb1efa702bc411
Konrad Linkowski <linkowski.konrad97@gmail.com>
2020-12-04 21:19:19 +0100

    2015 - Day6

---
+++ 2015/Day6/a.js
@@ -0,0 +1,32 @@
+const instructions = require('../../loader').split('\n').map(line => {
+  const [_, command, x1, y1, x2, y2] = line.match(/(on|off|toggle).*?(\d+),(\d+).*?(\d+),(\d+)/)
+  return { command, x1: +x1, y1: +y1, x2: +x2, y2: +y2 }
+})
+
+const grid = Array(1000).fill(0).map(() => Array(1000).fill(false))
+
+instructions.forEach(execute)
+
+console.log(getTurnedOn())
+
+function getTurnedOn() {
+  const s = (s, c) => s + c
+  return grid.map(row => row.reduce(s)).reduce(s)
+}
+
+function execute({ command, x1, y1, x2, y2 }) {
+  for (let y = y1; y <= y2; y += 1) {
+    for (let x = x1; x <= x2; x += 1) {
+      switch (command) {
+        case 'on':
+          grid[y][x] = true
+          break
+        case 'off':
+          grid[y][x] = false
+          break
+        case 'toggle':
+          grid[y][x] = !grid[y][x]
+      }
+    }
+  }
+}
