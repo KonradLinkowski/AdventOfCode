c3dfdfc7f5be631cf8fb533326c3a46031dae1ee
Konrad Linkowski <linkowski.konrad97@gmail.com>
2021-12-17 06:16:54 +0100

    2021 - Day17

---
+++ 2021/Day17/b.js
@@ -0,0 +1,34 @@
+const [minX, maxX, minY, maxY] = require('../../loader').match(/-?\d+/g).map(Number)
+
+let count = 0
+for (let y = -1000; y <= 1000; y += 1) {
+  for (let x = 1; x <= 1000; x += 1) {
+    const result = testProbe(x, y)
+    if (result != null) {
+      count += 1
+    }
+  }
+}
+
+console.log(count)
+
+function testProbe(startX, startY) {
+  let currentX = 0
+  let currentY = 0
+  let velocityX = startX
+  let velocityY = startY
+  let maxHeight = 0
+  while (currentX <= maxX && currentY >= minY) {
+    currentX += velocityX
+    currentY += velocityY
+    velocityX += velocityX < 0 ? 1 : velocityX > 0 ? -1 : 0
+    velocityY -= 1
+    if (currentY > maxHeight) {
+      maxHeight = currentY
+    }
+    if (currentX >= minX && currentX <= maxX && currentY >= minY && currentY <= maxY) {
+      return maxHeight
+    }
+  }
+  return null
+}
