382408d3bd912a875d9b7e766a46adf1b6d96aa7
Konrad Linkowski <linkowski.konrad97@gmail.com>
2022-12-04 13:30:12 +0100

    2022 - Day4

---
+++ 2022/Day4/a.js
@@ -0,0 +1,8 @@
+const data = require('../../loader');
+const pairs = data.split('\n').map(e => e.split(',').map(e => e.split('-').map(Number)));
+
+const final = pairs.reduce((count, [l, r]) => {
+  return count + (l[0] <= r[0] && l[1] >= r[1] || r[0] <= l[0] && r[1] >= l[1])
+}, 0)
+
+console.log(final)
