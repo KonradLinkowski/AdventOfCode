ac373e59ea22c2ecfaf1050cea9c794b5f83224a
Konrad Linkowski <linkowski.konrad97@gmail.com>
2023-12-09 16:24:13 +0100

    2023 - Day6

---
+++ 2023/Day6/a.js
@@ -0,0 +1,30 @@
+const [t, d] = require('../../loader').split('\n');
+
+const times = t.match(/\d+/g).map(Number);
+const distances = d.match(/\d+/g).map(Number);
+
+const races = times.map((time, i) => ({ time, record: distances[i] }));
+
+const results = races.map(({ time, record }) => {
+  let start, end;
+  for (let i = 1; i < time; i += 1) {
+    const distance = i * (time - i);
+    if (distance > record) {
+      start = i;
+      break;
+    }
+  }
+
+  for (let i = time - 1; i > 0; i -= 1) {
+    const distance = i * (time - i);
+    if (distance > record) {
+      end = i;
+      break;
+    }
+  }
+
+  return end - start + 1;
+});
+
+const sum = results.reduce((s, c) => s * c);
+console.log(sum);
